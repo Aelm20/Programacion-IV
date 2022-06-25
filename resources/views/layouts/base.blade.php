e1b1289f7197d19de2b3af5e89e203715e79fa17
Alexaris <aluna.1030@unimr.edu.ve>
2022-06-25 03:51:51 -0430

    Solucionados previos problemas, vista welcome list

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+    <html>
+
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+                <title> MaaS</title>
+                <link rel="stylesheet"  href="{!! asset('css/app.css') !!}" >
+
+            </head>
+
+      <body>
+            <!---------Encabezado-------->
+            <header>
+                <a href="">
+                <h2> Monitoring as a Services </h2>
+                </a>
+                    <nav class="menu">
+                        <ul>
+                            <a href="#" >Nuestro Equipo</a>
+                            <a href="#">Servicios</a>
+                            <a href="#">Registrate</a>
+
+                        </ul>
+                    </nav>
+            </header>
+
+            <!--------contenido---------->
+            <main class="container"
+                @yield('content')
+            </main>
+
+            <!--------Pie de pagina------->
+            <Footer>
+                <div class="footer-content">
+                 <h3>InformticsGuards SpA</h3>
+                    <nav>
+                         <ul>
+                             <a href="#" > Contáctanos </a><br/>
+                             <a href="#" >Acerca de nosotros</a>
+                        </ul>
+                    </nav>
+                <p>&copy;2022 . designed by Alexaris Luna</p>
+                </div>
+            </Footer>
+        </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,20 @@
-@extends('layout.layout')
+@extends('layouts.base')
+
+@section('- MaaS', 'Bienvenido')
 
 @section ('content')
 
-    <h1>HOLA</h1>
+    <link rel="stylesheet"  href="{!! asset('css/app.css') !!}" >
+    <section class="login">
+        <h3> Inciar Sesión</h3>
+            <input class="control" type="text" name="Usuaio o E-mail" value="" placeholder="Usuaio o E-mail">
+            <input class="control" type="password" name="Contraseña" value="" placeholder="Contraseña">
+            <input class="button" type="submit" name="" value="Ingresar">
+                <p>¿Quieres pertenecer a nuestro equipo? <br> <a href="#">Más información aquí</a></p>
+     </section>
 
 @endsection
+
 
 
 
