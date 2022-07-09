6e3b199dd2295356d33f7587434de2461c265d5d
Alexaris <aluna.1030@unimr.edu.ve>
2022-07-09 12:28:20 -0430

    Vista contact en proceso

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,14 @@
 
 @section ('content')
 
-    <h1>PRUEBA</h1>
-
+    <link rel="stylesheet"  href="{!! asset('css/app.css') !!}" >
+    <h3> Envianos tu informacion -MaaS-</h3>
+    <section class="contac">
+        <input class="control" type="text" name="Nombre y/o Empresa" value="" placeholder="Nombre y/o Empresa">
+        <input class="control" type="text" name="Telefono" value="" placeholder="Telefono">
+        <input class="control" type="text" name="E-mail" value="" placeholder="E-mail">
+        <input class="control" type="text" name="Tu mensaje" value="" placeholder="TU Mensaje">
+        <input class="button" type="submit" name="" value="Eviar">
+    </section>
 @endsection
 
