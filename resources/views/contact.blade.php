cf0a553bd1700e85198b44ef8444d5a040a67724
Alexaris <aluna.1030@unimr.edu.ve>
2022-07-09 14:33:55 -0430

    Avances, contact lista, welcome modificado el css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,13 +3,13 @@
 @section ('content')
 
     <link rel="stylesheet"  href="{!! asset('css/app.css') !!}" >
-    <h3> Envianos tu informacion -MaaS-</h3>
+    <h3> ¡Contactate con nosotros!</h3>
     <section class="contac">
-        <input class="control" type="text" name="Nombre y/o Empresa" value="" placeholder="Nombre y/o Empresa">
-        <input class="control" type="text" name="Telefono" value="" placeholder="Telefono">
-        <input class="control" type="text" name="E-mail" value="" placeholder="E-mail">
-        <input class="control" type="text" name="Tu mensaje" value="" placeholder="TU Mensaje">
-        <input class="button" type="submit" name="" value="Eviar">
+        <input class="controls" type="text" name="Nombre y/o Empresa" value="" placeholder=" Nombre y/o Empresa">
+        <input class="controls" type="text" name="Telefono" value="" placeholder=" Teléfono">
+        <input class="controls" type="text" name="E-mail" value="" placeholder=" E-mail">
+        <input class="controls" type="text" name="Tu mensaje" value="" placeholder=" Tu mensaje">
+        <input class="button-2" type="submit" name="" value=" Enviar">
     </section>
 @endsection
 
