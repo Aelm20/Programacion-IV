30364e825d86883257b0a4c19385e269cd3704e2
Alexaris <aluna.1030@unimr.edu.ve>
2022-07-10 11:12:14 -0430

    vista about en proceso

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,37 @@
-extends (
+@extends ('layouts.base')
+
+
+@section ('content')
+    <link rel="stylesheet"  href="{!! asset('css/app.css') !!}" >
+
+    <section class="about">
+        <img src="img/about.jpg" alt="quienes somos">
+        <h1>Acerca de InformaticsGuards SpA</h1></br>
+        <p>Es una empresa líder en soluciones de software</p>
+        <p>consagrada a la asesoría e implantación de soluciones</p>
+        <p>para ITSM y Seguridad Digital. InformaticsGuards SpA</p>
+        <p>cuenta con un portafolio de más de 1000 clientes</p>
+        <p>satisfechos en más de 30 países en América Latina,</p>
+        <p >el Caribe y los Estados Unidos.</p></br>
+
+        <p>Hacer lo correcto, hacer nuestro trabajo lo mejor que</p>
+        <p>podamos y mostrar a nuestros clientes, partners y</p>
+        <p>compañeros lo mucho que nos importan... Esos son los</p>
+        <p>valores que nos mueven en InformaticsGuards SpA.</p>
+    </section>
+
+    <section class="about-two">
+        <h1> InformormaticsGuards Spa</h1>
+        <p>Trabaja virtualmente en todos los paises de América</p>
+    </section>
+
+    <section class="about-three">
+        <h1>NUESTROS VALORES COMO</h1>
+        <H1>ORGANIZACIÓN</H1>
+        <img src="img/valores.jpg" alt="quienes somos">
+        <p>1. Hacer lo correcto.</p>
+        <p>2. Hacer lo mejor que podamos.</p>
+        <p>3. Demostrar a los demás que nos importan.</p>
+    </section>
+
+@endsection
