a37f672146da992e0edc07a9446d6f5435a74ae2
Alexaris <aluna.1030@unimr.edu.ve>
2022-07-13 08:24:12 -0430

    actualizacion

--- resources/views/nuestros_servicios.blade.php
+++ resources/views/nuestros_servicios.blade.php
@@ -2,7 +2,37 @@
 
 @section('content')
 
+        <section class="service">
+            <img src="img/services.jpg" alt="servicios">
+                <h1>RECIBE MÁS INFORMACIÓN SOBRE</h1>
+                <h1>NUESTROS PRODUCTOS</h1>
+                <h1 class="btm-1">Y SERVICIOS</h1><hr>
 
+                    <p class="tp">Te ofrecemos un gran variedad de productos y servicios</p>
+                    <p>orientados a satisfacer diversas necesidades de tu empresa.</p>
+                    <p>Echa vistazo a nuestra propuesta y descubre todo lo que podemos </p>
+                    <p>hacer por ti y tu organización.</p>
+        </section>
 
+        <section class="service-1">
+            <h1>Monitoring as a service</h1>
+        </section>
 
+        <section class="service-2">
+            <img src="img/maas.jpg" alt="servicios-1">
+            <h1>APROVECHA LAS VENTAJAS DE</h1>
+            <h1>MONITORING AS A SERVICE</h1><hr>
+
+            <p>¿Qué mejor manera de sacar provecho a tu herramienta que con</p>
+            <p>el respaldo de un equipo que está siempre disponible? Nuestro</p>
+            <p>servicio de asistencia remota te ofrece: </p>
+            <p>1. Atención inmediata a tus solicitudes</p>
+            <p>2. Asesoría de expertos</p>
+            <p>3. Conexión a través de herramientas que garantizan la privacidad de tus datos</p>
+            <p>4. Interacción en tiempo real con transferencia de conocimientos</p>
+        </section>
+
+        <section class="service-1">
+            <h1>SOLICITA TU PRESUPUESTO SIN COSTO</h1>
+        </section>
     @endsection
